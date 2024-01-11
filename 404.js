cf36b00c2efcd151af69089592e49b4ffedd293e
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2024-01-11 16:58:32 -0300

    fix 404

--- 404.js
+++ 404.js
@@ -1,8 +1,8 @@
 const back = document.getElementById('back-btn')
 
-if (window.innerWidth <= 390) {
+if (window.innerWidth <= 900) {
   back.innerText = 'Volver a intentar'
-} else if (window.innerWidth >= 390) {
+} else if (window.innerWidth >= 900) {
   back.innerText = 'Inicio/¿Volver a intentar?'
 }
 
